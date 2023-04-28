b83d434db848b72ef746bff54ffc92f364d00fc6
Chaeyoung Moon <s2123@e-mirim.hs.kr>
2023-04-28 15:13:06 +0900

    main 제작중

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,11 @@ function Main() {
     return(
     <div id={styles.first}>
         <div className={styles.blackBg}>
-
+            <div className="title">
+                <div className='de'>DEVELOPER PORTPOLIO</div>
+                <div className='hi'>Hello, World.</div>
+                <div className='name'>문채영입니다.</div>
+            </div>
         </div>
     </div>
     )
