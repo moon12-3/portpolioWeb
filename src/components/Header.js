7ab0725d02cead36c3827ef3139d3104c8ca9c7c
chaeyoung Moon <80873608+moon12-3@users.noreply.github.com>
2023-05-01 22:25:35 +0900

    Header 색 변경 구현

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,29 @@
 // import dummy from "../db/data.json";
 // import { Link } from 'react-router-dom';
 import {Link} from "react-router-dom";
+import { useLocation } from 'react-router-dom';
 import logo from "../img/logo.png";
-// import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 
 let s = (window.location.href).replace("http://localhost:3000", "");
-console.log(s);
 function Header(props) {
+    const loc = useLocation();
+
     return (
         <header>
             <div className ="nav">
                 <img className = "logo" src={logo}/>
                 <ul className="nav-list">
-                    <li><Link to='/'>MAIN</Link></li>
-                    <li><Link to='/skills'>SKILLS</Link></li>
-                    <li><Link to='/projects'>PROJECT</Link></li>
+                    <li><Link to='/'
+                        style={{ opacity: loc.pathname === "/" ? 1 : 0.4 }}
+                    >
+                        MAIN</Link></li>
+                    <li><Link to='/skills'
+                        style={{ opacity: loc.pathname === "/skills" ? 1 : 0.4 }}
+                    >SKILLS</Link></li>
+                    <li><Link to='/projects'
+                        style={{ opacity: loc.pathname === "/projects" ? 1 : 0.4 }}
+                    >PROJECT</Link></li>
                 </ul>
             </div>
         </header>
